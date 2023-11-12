73eb6d97dafbe37e8d3abf0f0b73824cad143f0d
Jimena Villanueva <49253521+jimevillanueva@users.noreply.github.com>
2023-11-12 20:33:49 -0300

    Add files via upload

---
+++ ejercicio2b/initScript.sh
@@ -0,0 +1,9 @@
+gcc client.c -lm -o cli
+vagrant up
+
+for i in {4..8}
+do
+  SERVER_PORT=$((8080 + $i))
+  SERVER_IP="192.168.0.200"
+  ./cli $SERVER_IP $SERVER_PORT $i >> resultados/cliente.txt
+done
